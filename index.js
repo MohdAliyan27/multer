31c83f199ecac9f92874f94d99a6f69fec6e7e25
Mohd Aliyan <71941136+MohdAliyan27@users.noreply.github.com>
2022-06-26 21:24:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require("express")
-const path = require("path")
-const multer = require("multer")
-const app = express()
+const express = require("express");
+const path = require("path");
+const multer = require("multer");
+const app = express();
 	
 // View Engine Setup
 app.set("views",path.join(__dirname,"views"))
